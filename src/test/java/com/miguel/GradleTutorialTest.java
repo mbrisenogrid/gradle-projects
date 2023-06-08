7ae4df67a0936cac7de45b861607877034427769
Miguel Angel Briseño Flores <mbriseno@griddynamics.com>
2023-06-07 18:07:30 -0600

    tests added

---
+++ src/test/java/com/miguel/GradleTutorialTest.java
@@ -0,0 +1,13 @@
+package com.miguel;
+
+import java.beans.Transient;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[]{});
+    }
+}
